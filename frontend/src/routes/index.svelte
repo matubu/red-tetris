896fd9de463e51237dd8e57d18ea9b62d5232e4c
Mathias Berger--fruhinsholz <throw_new_Error@protonmail.com>
2022-06-10 11:40:53 +0200

    change logo size

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -24,11 +24,9 @@ main {
 </style>
 
 <main>
-	<img class="logo-img" width="256" alt="logo" src="/red-tetris-3d.png">
+	<img class="logo-img" width="340" alt="logo" src="/red-tetris-3d.png">
 	<form class="card" on:submit={playButton}>
 		<input bind:value={$user} class="red-input" required placeholder="Enter an username">
-		<button class="red-button">
-			PLAY
-		</button>
+		<button class="red-button">PLAY</button>
 	</form>
 </main>
