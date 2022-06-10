6eefc54a97b90000160e768bd8a57cd9afa44258
Mathias Berger--fruhinsholz <throw_new_Error@protonmail.com>
2022-06-10 19:42:14 +0200

    disconnect

--- frontend/src/lib/user.js
+++ frontend/src/lib/user.js
@@ -2,13 +2,17 @@ import { writable } from "svelte/store";
 import { browser } from "$app/env";
 import { io } from "socket.io-client"
 
-export let user = writable()
-export const socket = io('http://localhost:4000');
+export let user = writable();
+export let connected = writable(true);
+export let socket;
 
 if (browser)
 {
 	user.set(localStorage.getItem('user'))
-	user.subscribe(username => {
-		localStorage.setItem('user', username)
-	})
+	user.subscribe(username => localStorage.setItem('user', username))
+
+	socket = io(`http://${location.hostname}:4000`)
+	socket.on("connect", () => connected.set(true))
+	socket.on("connect_error", () => connected.set(false));
+	socket.on("disconnect", () => connected.set(false));
 }
--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,5 +1,23 @@
 <script>
+	import { connected } from '$lib/user.js'
 	import '$lib/style.css'
 </script>
 
+<style>
+	.disconectted {
+		position: fixed;
+		width: 100%;
+		bottom: 0;
+		left: 0;
+		background: var(--back-1);
+		color: var(--text);
+		text-align: center;
+	}
+</style>
+
+{#if !$connected}
+<div class="disconectted">
+	disconectted
+</div>
+{/if}
 <slot />
