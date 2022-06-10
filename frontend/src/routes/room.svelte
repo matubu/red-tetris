b6fd46898c79e8905e59ea26606cb6622bd0ac3c
Mathias Berger--fruhinsholz <throw_new_Error@protonmail.com>
2022-06-11 00:06:15 +0200

    starting gameMode sync

--- frontend/src/routes/room.svelte
+++ frontend/src/routes/room.svelte
@@ -6,6 +6,9 @@
 
 	let roomname = ''
 	let users = []
+
+	let gameMode = 'earth'
+
 	onMount(() => {
 		if (!(roomname = location.hash.slice(1).toLowerCase()))
 			goto('/rooms')
@@ -23,9 +26,36 @@
 
 			socket.on('connect', joinRoom)
 			joinRoom()
+
+			socket.on(`gameMode:${roomname}`, (_gameMode) => {
+				gameMode = _gameMode
+			})
 		}
 	})
+
+	$: socket.emit(`gameMode:${roomname}`)
 </script>
+
+<style>
+	input[type=radio] {
+		display: none;
+	}
+	[name="gameMode"] + .red-button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: .1em;
+		width: 1em;
+	}
+	.red-button img {
+		width: 100%;
+	}
+	input[type=radio]:not(:checked) + .red-button {
+		--back-1: var(--grey-back-1);
+		--border-1: var(--grey-border-1);
+		--shadow: var(--grey-border-0);
+	}
+</style>
 
 <main class="main">
 	<div id="logo" on:click={() => {
@@ -37,8 +67,25 @@
 	<div class="card">
 		<h1>{roomname}</h1>
 		{#each users as user}
-			<p>{user}</p>
+		<p>- {user}</p>
 		{/each}
+		<div class="action">
+			<input type="radio" bind:group={gameMode} name="gameMode"
+				value="moon" id="moon" />
+			<label class="red-button" for="moon">
+				<img src="/game-mode/moon.png" alt="">
+			</label>
+			<input type="radio" bind:group={gameMode} name="gameMode"
+				value="earth" id="earth" />
+			<label class="red-button" for="earth">
+				<img src="/game-mode/earth.png" alt="">
+			</label>
+			<input type="radio" bind:group={gameMode} name="gameMode"
+				value="sun" id="sun" />
+			<label class="red-button" for="sun">
+				<img src="/game-mode/sun.png" alt="">
+			</label>
+		</div>
 		<div class="action">
 			<button
 				class="red-button"
