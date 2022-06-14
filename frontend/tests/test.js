5a1ae83582cf0dc52be4f3358ff7343bb42dbd40
Mathias Berger--fruhinsholz <throw_new_Error@protonmail.com>
2022-06-15 00:14:30 +0200

    test

--- frontend/tests/test.js
+++ frontend/tests/test.js
@@ -5,3 +5,19 @@ test('index page has input', async ({ page }) => {
 
 	await expect(page.locator('input').first()).toBeVisible();
 });
+
+test('index page has button', async ({ page }) => {
+	await page.goto('/');
+
+	let button = page.locator('button').first()
+	await expect(button).toBeVisible();
+	await expect(button).toHaveText('PLAY');
+})
+
+test('login', async ({ page }) => {
+	await page.goto('/');
+
+	await page.fill('input', 'myname');
+	await page.click('button');
+	await page.screenshot({path: 'screenshot.png'});
+})
