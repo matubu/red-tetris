cbba7d14fc83d91ce21991dba27be209bad879fe
Mathias Berger--fruhinsholz <throw_new_Error@protonmail.com>
2022-06-15 00:03:13 +0200

    change config

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -2,11 +2,8 @@
 const config = {
 	webServer: {
 		command: "npm run dev",
-		url: 'http://localhost:3000/',
+		port: 3000,
 		reuseExistingServer: true
-	},
-	use: {
-		baseURL: 'http://localhost:3000/',
 	}
 };
 
